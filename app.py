c31532e083c32a5c9bdef054a3c76617960753ec
Eduardo <ehernandezvilla@gmail.com>
2023-06-21 15:55:39 -0400

    Adding gitignore

--- app.py
+++ app.py
@@ -5,14 +5,27 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from bottle import route
 from flask import jsonify
+from models import db, setup_db, Domains 
+
+
+database_name = "FinalFSND"
+database_path = "postgresql://{}:{}@{}/{}".format('postgres', 'donip123', 'localhost:5432', database_name)
+
 
 # Function that create the app and return it
 def create_app(test_config=None):
     # Create and configure the app 
     app = Flask(__name__)
-    CORS(app)
+    app.app_context().push()
+     
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    migrate = Migrate(app, db, render_as_batch=False)
+    CORS(app)  
+    db.init_app(app)
 
-
+    # Create the routes 
     @app.route('/')
     def index():
         return 'Hello World!'
