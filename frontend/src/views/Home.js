89d81b9141e14e8ee4fc7fc2620d8f68f5c43ba0
Eduardo <ehernandezvilla@gmail.com>
2023-06-27 20:21:19 -0400

    Adding styles and footer to Home

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,11 +1,16 @@
 // src/views/Home.js
 import React from "react";
+import "./Home.css"; // Importar el archivo CSS local
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Home</h1>
       <p>Hey! Welcome to the FSND home project!</p>
+      <p>This is a simple CRUD app built with React and Flask.</p>
+      <footer className="footer">
+        <p>&copy; 2023 ehernandezvilla FSND Project. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
