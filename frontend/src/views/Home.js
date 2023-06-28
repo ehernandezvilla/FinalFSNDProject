d53be9f913511cfe9cefb4aaff06d2108ad92290
Eduardo <ehernandezvilla@gmail.com>
2023-06-27 23:05:35 -0400

    Small text modifications

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
         <p>Hey! Welcome to the FSND home project!</p>
         <p>This is a simple CRUD app built with React and Flask.</p>
       </div> 
-      <h2 className="heading-text">What do we do</h2>
+      <h2 className="heading-text">¿How can we help you?</h2>
       <div className="grid-container">
         <div>
           <img src={icon1} alt="Icon 1" className="icon" />
