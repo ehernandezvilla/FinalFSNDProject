124cd0b614d07dd797f8c433cdb8efd803eda334
Eduardo <ehernandezvilla@gmail.com>
2023-06-29 13:05:26 -0400

    fixing PhishingSearch comp and adding styles

--- frontend/src/views/PhishingSearch.js
+++ frontend/src/views/PhishingSearch.js
@@ -4,18 +4,42 @@ import './PhishingSearch.css';
 
 const PhishingSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [phishingResult, setPhishingResult] = useState(null);
+  const [phishingResult, setPhishingResult] = useState([]);
   const [error, setError] = useState(null);
 
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('/phishing/search', { search_term: searchTerm }, { headers: { 'Content-Type': 'application/json' } });
-      setPhishingResult(response.data);
+      const response = await axios.post(
+        'http://127.0.0.1:5000/phishing/search',
+        { search_term: searchTerm },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      // Limitar la cantidad de resultados mostrados a 3
+      const limitedResults = response.data.data.slice(0, 3);
+      setPhishingResult(limitedResults);
       setError(null);
     } catch (error) {
-      setPhishingResult(null);
+      setPhishingResult([]);
+      setError('Error fetching phishing result: ' + error.message);
+    }
+  };
+
+  const handleLoadMore = async () => {
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:5000/phishing/search',
+        { search_term: searchTerm },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      // Obtener los resultados adicionales a partir del cuarto elemento
+      const additionalResults = response.data.data.slice(3);
+      setPhishingResult((prevResults) => [...prevResults, ...additionalResults]);
+      setError(null);
+    } catch (error) {
       setError('Error fetching phishing result: ' + error.message);
     }
   };
@@ -23,33 +47,47 @@ const PhishingSearch = () => {
   return (
     <div className="phishing-search-container">
       <form className="search-form" onSubmit={handleSearchSubmit}>
-        <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Enter domain to search" />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Enter domain to search"
+        />
         <button type="submit">Search</button>
       </form>
 
       {error && <p className="error-message">{error}</p>}
 
-      {phishingResult && (
+      {phishingResult.length > 0 && (
         <div className="result-container">
           <h3 className="result-title">Phishing Result:</h3>
-          <p>
-            <strong>Create Date:</strong> {phishingResult.create_date}
-          </p>
-          <p>
-            <strong>Description:</strong> {phishingResult.description}
-          </p>
-          <p>
-            <strong>Is Dangerous:</strong> {phishingResult.is_dangerous ? 'Yes' : 'No'}
-          </p>
-          <p>
-            <strong>IP Address:</strong> {phishingResult.ip}
-          </p>
-          <p>
-            <strong>Phishing URL:</strong> {phishingResult.phishing_url}
-          </p>
-          <p>
-            <strong>Submitted by:</strong> {phishingResult.submitted_by}
-          </p>
+          {phishingResult.map((result) => (
+            <div key={result.id} className="phishing-item">
+              <p>
+                <strong>Create Date:</strong> {result.create_date}
+              </p>
+              <p>
+                <strong>Description:</strong> {result.description}
+              </p>
+              <p>
+                <strong>Is Dangerous:</strong> {result.is_dangerous ? 'Yes' : 'No'}
+              </p>
+              <p>
+                <strong>IP Address:</strong> {result.ip}
+              </p>
+              <p>
+                <strong>Phishing URL:</strong> {result.phishing_url}
+              </p>
+              <p>
+                <strong>Submitted by:</strong> {result.submitted_by}
+              </p>
+            </div>
+          ))}
+          {phishingResult.length > 3 && (
+            <p className="load-more">
+              <button onClick={handleLoadMore}>Load More</button>
+            </p>
+          )}
         </div>
       )}
     </div>
@@ -57,6 +95,13 @@ const PhishingSearch = () => {
 };
 
 export default PhishingSearch;
+
+
+
+
+
+
+
 
 
 
