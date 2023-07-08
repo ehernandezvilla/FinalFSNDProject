6f72590869b04bc0943fbdd0b7950d2273baebf1
Eduardo <ehernandezvilla@gmail.com>
2023-07-08 16:00:19 -0400

    addding test_app for unittest

---
+++ backend/test_app.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+import json
+from models import db
+from app import db 
+from app import create_app
+from decouple import config 
+from models import setup_db, Domains, Phishing, Articles
+
+class DomainsTestCase(unittest.TestCase):
+    """This class represents the domains test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = config('DB_NAME')
+        self.database_path = "postgresql://{}:{}@{}/{}".format('postgres', config('PASSWORD'), 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+        self.db = db
+
+        # binds the app to the current context
+
+    def tearDown(self):
+        """Executed after reach test"""
+
+        pass 
+
+    def test_get_domains(self):
+        res = self.client().get('/domains')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['domains'])
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
