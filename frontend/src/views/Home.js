67789623e7d184290286062dcc4d11b2cafae4d3
Eduardo <ehernandezvilla@gmail.com>
2023-07-08 13:46:38 -0400

    style changes

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,9 +1,10 @@
 // src/views/Home.js
 import React from 'react';
 import logo from '../assets/images/logo.jpg';
-import icon1 from '../assets/images/17.-Cloud.svg';
-import icon2 from '../assets/images/23.-Going-Online.svg';
-import icon3 from '../assets/images/22.-Cyber-Security.svg';
+import icon1 from '../assets/images/icon1.png';
+import icon2 from '../assets/images/icon2.png';
+import icon3 from '../assets/images/icon3.png';
+import icon4 from '../assets/images/icon4.png';
 import './Home.css'; // Importamos el archivo de estilos CSS
 import Footer from './Footer'; // Importamos el componente Footer
 import Articles from './Articles';
@@ -27,29 +28,29 @@ const Home = () => {
         <div>
           <img src={icon1} alt="Icon 1" className="icon" />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur libero eget arcu rhoncus
-            facilisis. Nulla facilisi.
+          Phishing is when attackers attempt to trick users into doing 'the wrong thing', such as clicking a bad link that will download malware, or direct them to a dodgy website..
           </p>
         </div>
         <div>
           <img src={icon2} alt="Icon 2" className="icon" />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur libero eget arcu rhoncus
-            facilisis. Nulla facilisi.
+          Most phishing attacks are sent by email. The crook will register a fake domain that mimics a genuine organisation and sends thousands of generic requests. 
+The fake domain often involves character substitution, like using ‘r’ and ‘n’ next to each other to create ‘rn’ instead of ‘m’. 
           </p>
         </div>
         <div>
           <img src={icon3} alt="Icon 3" className="icon" />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur libero eget arcu rhoncus
-            facilisis. Nulla facilisi.
+          The recipient might see the word ‘Amazon’ in the sender’s address and assume that it was a genuine email.
+There are many ways to spot a phishing email, but as a general rule, you should always check the email address of a message that asks you to click a link or download an attachment. 
           </p>
         </div>
         <div>
-          <img src={icon1} alt="Icon 1" className="icon" />
+          <img src={icon4} alt="Icon 1" className="icon" />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur libero eget arcu rhoncus
-            facilisis. Nulla facilisi.
+          Whaling attacks are even more targeted, taking aim at senior executives. Although the end goal of whaling is the same as any other kind of phishing attack, the technique tends to be a lot subtler. 
+Tricks such as fake links and malicious URLs aren’t helpful in this instance, as criminals are attempting to imitate senior staff. 
+Whaling emails also commonly use the pretext of a busy CEO who wants an employee to do them a favour.
           </p>
         </div>
       </div>
